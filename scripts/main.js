5da00e289a62250e485a7bfcd3085bac6ccbd38a
Michał Apanowicz <michal.zhr@gmail.com>
2019-08-20 19:24:50 +0200

    2. Primitive types

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,35 @@
 console.log("Hello JS!")
+
+
+// #region Typy proste
+
+// numbers
+var number = 1000; // or 12.3
+
+var x = 0.2;
+var y = 0.1;
+
+var z = x+y === 0.3;
+
+// bool
+var boolean = true; // or false
+
+// string
+var string = "string";
+
+// undefined
+var undefined_variable = undefined;
+
+// #region es6
+
+// symbol
+var symbol1 = new Symbol("symbol");
+var symbol2 = new Symbol("symbol");
+
+console.log(symbol == symbol2); //false
+
+// bigint
+var bigint = 100n;
+// #endregion
+
+// #endregion
