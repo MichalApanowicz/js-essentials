75df3722e5616f937a58a65d83fc8d26efc53be3
ItsMeaga1n <peinpat1@gmail.com>
2019-08-20 19:48:14 +0200

    3. Reference type(s)

--- scripts/main.js
+++ scripts/main.js
@@ -23,13 +23,57 @@ var undefined_variable = undefined;
 // #region es6
 
 // symbol
-var symbol1 = new Symbol("symbol");
-var symbol2 = new Symbol("symbol");
+var symbol1 = Symbol("symbol");
+var symbol2 = Symbol("symbol");
 
-console.log(symbol == symbol2); //false
+console.log(symbol1 == symbol2); //false
 
 // bigint
 var bigint = 100n;
 // #endregion
+
+// #endregion
+
+// #region Typy referencyjne
+
+var object = {};
+
+console.log(object === {});
+
+object = {
+    property: "property"
+}
+
+var newObject = object;
+
+object.newProperty = "new property"
+
+console.log(object.newProperty, newObject.newProperty, object["newProperty"]);
+
+var array = ["item1", 2, false];
+console.log(array.length);
+
+var add = function (arg1, arg2) {
+    return arg1 + arg2;
+};
+
+
+console.log(add);
+console.log(add(1, 2));
+console.log(add("Hello", " functions!"));
+console.log(add("Hello", 1));
+
+object.sayMyName = function () {
+    console.log(`My name is ${this.name}`);
+};
+object.name = "Pat";
+
+object.sayMyName();
+
+var objectFromObject = Object.create(object);
+objectFromObject.name = "Not Pat";
+
+object.sayMyName();
+objectFromObject.sayMyName();
 
 // #endregion
