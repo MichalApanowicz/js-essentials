b04ce0d7bfed4ee4dc27b17bfacf17fd4125b63c
Michał Apanowicz <michal.zhr@gmail.com>
2019-08-20 20:28:56 +0200

    7. RoadPrinter

---
+++ scripts/roadPrinter.js
@@ -0,0 +1,24 @@
+var road = {
+   /*
+    ...
+   */
+}
+
+
+// #region Ex 1
+road.setLength(10)
+    .print();
+
+// output:
+// __________
+// #endregion
+
+// #region Ex2
+road.setLength(60)
+    .setCarAt(20)
+    .print();
+
+// output:
+// ____________________🚗_______________________________________
+
+// #endregion 
